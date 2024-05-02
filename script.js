5f9851a84109f3dbf1a6fc95c1b7af3d81b8a1ca
Yimumin42220303 <129664991+Yimumin42220303@users.noreply.github.com>
2024-05-02 16:05:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function copyToClipboard(elementId) {
+    var copyText = document.getElementById(elementId);
+    copyText.select(); // 입력 필드 선택
+    document.execCommand("copy"); // 텍스트 복사
+    alert("Copied the text: " + copyText.value); // 사용자에게 복사 완료 알림
+}
+
 function calculateTrade() {
     var entryPrice = parseFloat(document.getElementById('entryPrice').value);
     var stopLossPrice = parseFloat(document.getElementById('stopLossPrice').value);
@@ -8,7 +15,5 @@ function calculateTrade() {
     var percentLoss = priceDifference / entryPrice;
     var orderAmount = Math.abs(riskAmount / percentLoss); // 주문량 (USDT)의 절대값
 
-    document.getElementById('result').innerHTML = `
-        주문량: ${orderAmount.toFixed(2)} USDT
-    `;
+    document.getElementById('result').innerHTML = `주문량: ${orderAmount.toFixed(2)} USDT`;
 }
