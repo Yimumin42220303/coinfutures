9f3fb561e114b2d0a64b9a7613e0b6a250ef056d
Yimumin42220303 <129664991+Yimumin42220303@users.noreply.github.com>
2024-05-02 16:45:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function calculateTrade() {
     var percentLoss = priceDifference / entryPrice;
     var orderAmount = Math.abs(riskAmount / percentLoss);  // 주문량을 절대값으로 계산
 
-    // '주문 값'을 결과 앞에 추가
     document.getElementById('result').innerText = `주문 값: ${orderAmount.toFixed(2)} USDT`;
     document.getElementById('copyButton').style.display = 'block'; // 계산 후 복사하기 버튼 표시
 }
@@ -19,7 +18,11 @@ function copyToClipboard() {
     var numericValue = content.replace(/[^\d.-]/g, ''); // "USDT" 및 '주문 값:' 텍스트를 제외하고 숫자만 추출
     navigator.clipboard.writeText(numericValue)
         .then(() => {
-            alert('클립보드에 복사되었습니다: ' + numericValue);
+            let confirmDiv = document.getElementById('copyConfirm');
+            confirmDiv.style.display = 'block';
+            setTimeout(() => {
+                confirmDiv.style.display = 'none';
+            }, 2000); // 2초 후 메시지가 사라지도록 설정
         })
         .catch(err => {
             console.error('클립보드 복사에 실패하였습니다.', err);
