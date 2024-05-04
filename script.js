67cde840a455ea71bc57bd241df312b9de794944
Yimumin42220303 <129664991+Yimumin42220303@users.noreply.github.com>
2024-05-04 14:32:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,29 +6,25 @@ function calculateTrade() {
     var riskAmount = seedAmount * 0.05;
     var priceDifference = entryPrice - stopLossPrice;
     var percentLoss = priceDifference / entryPrice;
-    var orderAmount = Math.abs(riskAmount / percentLoss);  
+    var orderAmount = Math.abs(riskAmount / percentLoss);  // 주문량을 절대값으로 계산
 
     document.getElementById('result').innerText = `주문 값: ${orderAmount.toFixed(2)} USDT`;
-    document.getElementById('copyButton').style.display = 'block';
+    document.getElementById('copyButton').style.display = 'block'; // 계산 후 복사하기 버튼 표시
 }
 
-// 입력값 복사 기능 (입력 필드 ID를 인자로 받음)
-function copyValue(inputId) {
-    var inputValue = document.getElementById(inputId).value;
-    navigator.clipboard.writeText(inputValue)
+// 클립보드로 복사 기능
+function copyToClipboard() {
+    var content = document.getElementById('result').innerText;
+    var numericValue = content.replace(/[^\d.-]/g, ''); // "USDT" 및 '주문 값:' 텍스트를 제외하고 숫자만 추출
+    navigator.clipboard.writeText(numericValue)
         .then(() => {
-            showCopyConfirm();
+            let confirmDiv = document.getElementById('copyConfirm');
+            confirmDiv.style.display = 'block';
+            setTimeout(() => {
+                confirmDiv.style.display = 'none';
+            }, 2000); // 2초 후 메시지가 사라지도록 설정
         })
         .catch(err => {
             console.error('클립보드 복사에 실패하였습니다.', err);
         });
-}
-
-// 클립보드 복사 확인 메시지 표시
-function showCopyConfirm() {
-    let confirmDiv = document.getElementById('copyConfirm');
-    confirmDiv.style.display = 'block';
-    setTimeout(() => {
-        confirmDiv.style.display = 'none';
-    }, 2000);
 }
