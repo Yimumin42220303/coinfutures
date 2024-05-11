4a00bcd0dc29556768c5affd6417b85da6df0252
Yimumin42220303 <129664991+Yimumin42220303@users.noreply.github.com>
2024-05-11 10:48:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,37 @@ function calculateTrade() {
     var stopLossPrice = parseFloat(document.getElementById('stopLossPrice').value);
     var seedAmount = parseFloat(document.getElementById('seedAmount').value);
 
-    // 주문 값 계산
     var riskAmount = seedAmount * 0.05;
     var priceDifference = entryPrice - stopLossPrice;
     var percentLoss = priceDifference / entryPrice;
-    var orderAmount = Math.abs(riskAmount / percentLoss);  
-
-    // 반익절라인 계산
+    var orderAmount = Math.abs(riskAmount / percentLoss);
     var breakevenLine = entryPrice + (entryPrice - stopLossPrice) * 1.5;
 
-    // 결과 표시
-    document.getElementById('result').innerHTML = `주문 값: ${orderAmount.toFixed(2)} USDT<br>반익절라인@손익비1:5(USDT) : ${breakevenLine.toFixed(2)}`;
-    document.getElementById('copyButton').style.display = 'block';
+    document.getElementById('result').innerHTML = `주문 값: ${orderAmount.toFixed(2)} USDT<br>반익절라인@손익비1:5(USDT): ${breakevenLine.toFixed(2)}`;
+    document.getElementById('copyOrderAmount').style.display = 'block';
+    document.getElementById('copyBreakevenLine').style.display = 'block';
+}
+
+function copyToClipboard(elementId) {
+    var text = '';
+    if (elementId === 'orderAmount') {
+        text = document.getElementById('result').innerText.split('USDT')[0].split(': ')[1].trim() + ' USDT';
+    } else if (elementId === 'breakevenLine') {
+        text = document.getElementById('result').innerText.split('USDT')[1].split(': ')[1].trim() + ' USDT';
+    }
+    navigator.clipboard.writeText(text)
+        .then(() => {
+            showCopyConfirm();
+        })
+        .catch(err => {
+            console.error('클립보드 복사에 실패하였습니다.', err);
+        });
+}
+
+function showCopyConfirm() {
+    let confirmDiv = document.getElementById('copyConfirm');
+    confirmDiv.style.display = 'block';
+    setTimeout(() => {
+        confirmDiv.style.display = 'none';
+    }, 2000);
 }
