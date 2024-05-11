7648aa44b1304e1da881075d524620820c101871
Yimumin42220303 <129664991+Yimumin42220303@users.noreply.github.com>
2024-05-11 10:53:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
-function calculateTrade() {
-    var entryPrice = parseFloat(document.getElementById('entryPrice').value);
-    var stopLossPrice = parseFloat(document.getElementById('stopLossPrice').value);
-    var seedAmount = parseFloat(document.getElementById('seedAmount').value);
-
-    var riskAmount = seedAmount * 0.05;
-    var priceDifference = entryPrice - stopLossPrice;
-    var percentLoss = priceDifference / entryPrice;
-    var orderAmount = Math.abs(riskAmount / percentLoss);
-    var breakevenLine = entryPrice + (entryPrice - stopLossPrice) * 1.5;
-
-    document.getElementById('result').innerHTML = `주문 값: ${orderAmount.toFixed(2)} USDT<br>반익절라인@손익비1:5(USDT): ${breakevenLine.toFixed(2)}`;
-    document.getElementById('copyOrderAmount').style.display = 'block';
-    document.getElementById('copyBreakevenLine').style.display = 'block';
-}
-
 function copyToClipboard(elementId) {
+    var content = document.getElementById('result').innerText;
     var text = '';
     if (elementId === 'orderAmount') {
-        text = document.getElementById('result').innerText.split('USDT')[0].split(': ')[1].trim() + ' USDT';
+        // 주문 값에서 숫자만 추출
+        text = content.split('USDT')[0].split(': ')[1].trim();
     } else if (elementId === 'breakevenLine') {
-        text = document.getElementById('result').innerText.split('USDT')[1].split(': ')[1].trim() + ' USDT';
+        // 반익절라인에서 숫자만 추출
+        text = content.split('USDT')[1].split(': ')[1].trim();
     }
+
     navigator.clipboard.writeText(text)
         .then(() => {
             showCopyConfirm();
